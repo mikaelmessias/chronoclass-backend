e74607f528f3fa9dc591cccc008cdb48c8a54c09
Mikael Messias <mikaelmessias@outlook.com>
2020-07-01 22:48:36 -0300

    Update Course update route
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/controllers/CourseController.ts
+++ src/controllers/CourseController.ts
@@ -1,9 +1,9 @@
 import { Ref } from '@typegoose/typegoose';
 import { Request, Response } from 'express';
 import CourseSchema from '../models/Course';
-import { Period } from '../models/Period';
-import { Teacher } from '../models/Teacher';
-// import TeacherSchema, { Teacher } from '../models/Teacher';
+import PeriodSchema, { Period } from '../models/Period';
+import TeacherSchema, { Teacher } from '../models/Teacher';
+import { Weekday } from '../utils/enums';
 
 interface CourseCreateBody {
   code: string,
@@ -56,22 +56,74 @@ class CourseController {
 
   async update(request: Request, response: Response): Promise<Response<any>> {
     const { courseId } = request.params;
-    const courseData: CourseUpdateBody = request.body;
 
     try {
+      const courseData: CourseUpdateBody = request.body;
+      const { periodsId, teacherId } = courseData;
+      let courseWeekdays: Weekday[] = [];
+
+      if (periodsId) {
+        courseWeekdays = await PeriodSchema.find({
+          _id: { $in: periodsId },
+        }, (_, periods) => {
+          if (periods?.length !== periodsId.length) {
+            return response.status(404).json({
+              error: 'Document not found for one or more given ids',
+            });
+          };
+
+          return true;
+        }).map((array) => (
+          array.map((object) => (
+            object.weekday
+          ))
+        ));
+      }
+
+      if (teacherId) {
+        const teacher = await TeacherSchema.findById(teacherId);
+
+        if (!teacher) {
+          return response.status(404).json({
+            error: 'Document not found for given teacher id',
+            teacherId,
+          });
+        }
+
+        if (courseWeekdays.length > 0) {
+          let unavailableDays: Weekday[] = [];
+
+          courseWeekdays.map((day) => {
+            if (!teacher.availableDays.includes(day)) {
+              unavailableDays.push(day);
+            }
+          });
+
+          unavailableDays = unavailableDays.filter((day, id) => unavailableDays.indexOf(day) === id);
+
+          if (unavailableDays.length > 0) {
+            return response.status(400).json({
+              error: 'Teacher not available for one or more weekdays',
+              weekdays: unavailableDays,
+            });
+          }
+        }
+      }
+
       const course = await CourseSchema.findOneAndUpdate(
         { _id: courseId }, courseData, { new: true },
-      );
+      )
+        .populate('periodsId', 'weekdayPeriod weekday')
+        .exec();
 
       if (!course) {
-        return response.status(400).json({
+        return response.status(404).json({
           error: 'Document not found for given ID.',
         });
       }
 
       return response.json(course);
     } catch (error) {
-      // console.log(error);
       return response.status(400).json(error);
     }
   }
