31b29f93c92bcc8dd07a21ee4b9d50420b04a9c4
Mikael Messias <mikaelmessias@outlook.com>
2020-07-02 21:08:29 -0300

    Refactor TeacherController
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/controllers/TeacherController.ts
+++ src/controllers/TeacherController.ts
@@ -7,7 +7,7 @@ interface TeacherBodyParams {
   name: string;
   email: string;
   area?: string;
-  availableDays: string | Weekday[];
+  availableDays: string;
 }
 
 interface TeacherQueryParams {
@@ -67,7 +67,10 @@ class TeacherController {
         });
       }
 
-      const courses = await CourseSchema.find({ teacherId }).populate('periodsId', 'weekday, weekdayPeriod').exec();
+      const courses = await CourseSchema
+        .find({ teacherId })
+        .populate('periodsId', 'weekday, weekdayPeriod')
+        .exec();
 
       return response.json({
         teacher,
@@ -79,31 +82,38 @@ class TeacherController {
   }
 
   async store(request: Request, response: Response): Promise<Response<any>> {
-    const {
-      name, email, area, availableDays,
-    }: TeacherBodyParams = request.body;
-
-    let serializedAvailableDays: Weekday[];
-
-    if (typeof availableDays === 'string') {
-      serializedAvailableDays = availableDays
-        .split(',')
-        .map((day) => Weekday[day.trim().toUpperCase() as keyof typeof Weekday]);
-    } else {
-      serializedAvailableDays = availableDays;
-    }
+    const teacherData: TeacherBodyParams[] = request.body;
 
     try {
-      const teacher = await TeacherSchema.create({
-        name,
-        email,
-        area,
-        availableDays: serializedAvailableDays,
+      const serializedTeacherData = teacherData.map((teacher) => {
+        if (!teacher.availableDays) {
+          throw new TypeError('availableDays is undefined');
+        }
+
+        const serializedAvailableDays: Weekday[] = teacher.availableDays
+          .split(',')
+          .map((day) => Weekday[
+              day.trim().toUpperCase() as keyof typeof Weekday
+          ]);
+
+        return {
+          ...teacher,
+          availableDays: serializedAvailableDays,
+        };
       });
 
-      return response.json(teacher);
+      const teachers = await TeacherSchema.create(serializedTeacherData);
+
+      return response.json(teachers);
     } catch (error) {
-      return response.status(400).json(error);
+      return response.status(400).json({
+        error: {
+          name: error.name,
+          message: error.code === 11000 ? 'Duplicate key' : error.message,
+          key: error.code === 11000 ? error.keyValue : null,
+          collection: error.code === 11000 ? 'chronoclass.teachers' : null,
+        },
+      });
     }
   }
 }
